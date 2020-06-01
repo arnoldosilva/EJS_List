4a2d8a076f45bbdd34a55e081d3cdbb1d2e3f2e3
Arnoldo Silva <analisi@gmail.com>
2020-06-01 18:23:56 -0300

    heroku ports config

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.get("/about", function(req,res){
     res.render("about")
 })
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Running on 3000 port")
 })
