0089e9f1aee3a6af5ca2d399e84dde39aa5b4675
Arnoldo Silva <analisi@gmail.com>
2020-06-01 17:44:08 -0300

    ejs templates config

--- app.js
+++ app.js
@@ -5,27 +5,46 @@ const bodyParser = require('body-parser')
 const app = express()
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended:true}))
-
-var items = []
+app.use(express.static("public"))
+let items = []
+let workItems = []
 
 app.get("/", function(req,res){
-    var today = new Date()
-    var options = {
+    let today = new Date()
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     }
-    var day = today.toLocaleDateString("pt-BR",options)
+    let day = today.toLocaleDateString("pt-BR",options)
     
-    res.render("list", {day:day, newListItems:items})
+    res.render("list", {ListTitle:day, newListItems:items})
 })
 
-app.post("/",function(req, res){
-    item = req.body.newItem
-    items.push(item)
-    res.redirect("/")
+
+app.post("/", function(req,res){
+    
+    let item = req.body.newItem
+
+    if (item != "") {
+        if (req.body.list === "Work") {
+            workItems.push(item)        
+            res.redirect("/work")
+        }else{
+            items.push(item)
+            res.redirect("/")
+        }      
+    }  
+    
 })
 
+app.get("/work", function(req,res){
+    res.render("list", {ListTitle:"Work List", newListItems: workItems})
+})
+
+app.get("/about", function(req,res){
+    res.render("about")
+})
 
 app.listen(3000,function(){
     console.log("Running on 3000 port")
